09206abe273154844312532d8de7a1a5845cd054
Sanvi <96112662+Bats34@users.noreply.github.com>
2023-04-28 15:32:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+song="";
+song2="";
+function preload() {
+   song=loadSound("music.mp3");
+   song2=loadSound("music2.mp3");
+}
+function setup() {
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',Gotposes);
+}
+function draw() {
+    image(video,0,0,600,500);
+}
+function modelLoaded() {
+    console.log("Model has loaded!!");
+} 
